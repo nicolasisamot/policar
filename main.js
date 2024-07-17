b466e9f3a9dc9946b1f4c686ffa062c229e1e923
nicolasisamot <nicolasisamot@gmail.com>
2024-07-16 21:06:28 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -4,19 +4,40 @@ const btFecharMenuHamburguer = document.getElementById(
 );
 const iconeBtMenuHamburguer = document.querySelector(".botao-menu-hamburguer");
 const menuLateral = document.querySelector(".menu-lateral");
+const listaBtMenuLateral = document.querySelectorAll(
+  ".lista-menu-lateral li a"
+);
+
+listaBtMenuLateral.forEach((botao) => {
+  botao.addEventListener("click", (event) => {
+    if (menuLateral.classList.contains("display-none")) {
+      removeMenuLateral();
+    }
+    if (menuLateral.classList.contains("display-block")) {
+      adicionaMenuLateral();
+    }
+  });
+});
 
 btMenuHamburguer.addEventListener("click", (evento) => {
   if (menuLateral.classList.contains("display-none")) {
-    menuLateral.classList.remove("display-none");
-    menuLateral.classList.add("display-block");
-    iconeBtMenuHamburguer.classList.add("display-none");
+    removeMenuLateral();
   }
 });
 
 btFecharMenuHamburguer.addEventListener("click", (evento) => {
   if (menuLateral.classList.contains("display-block")) {
-    menuLateral.classList.remove("display-block");
-    menuLateral.classList.add("display-none");
-    iconeBtMenuHamburguer.classList.remove("display-none");
+    adicionaMenuLateral();
   }
 });
+
+function removeMenuLateral() {
+  menuLateral.classList.remove("display-none");
+  menuLateral.classList.add("display-block");
+  iconeBtMenuHamburguer.classList.add("display-none");
+}
+function adicionaMenuLateral() {
+  menuLateral.classList.remove("display-block");
+  menuLateral.classList.add("display-none");
+  iconeBtMenuHamburguer.classList.remove("display-none");
+}
